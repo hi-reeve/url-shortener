c3332d53af7dac3441d9f047ac2576d1d34308cb
Christopher Reeve <reeveworkmail@gmail.com>
2021-09-17 16:01:46 +0700

    update

---
+++ README.md
@@ -0,0 +1,51 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [Shortly URL shortening API Challenge challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+1. [Frontend Mentor - Shortly URL shortening API Challenge solution](#frontend-mentor---shortly-url-shortening-api-challenge-solution)
+	1. [Table of contents](#table-of-contents)
+		1. [The challenge](#the-challenge)
+		2. [Links](#links)
+		3. [Built with](#built-with)
+	2. [Author](#author)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- Shorten any valid URL
+- See a list of their shortened links, even after refreshing the browser
+- Copy the shortened link to their clipboard in a single click
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [TailwindCss](https://tailwindcss.com/) - Css utilities framework
+- [Vitejs](https://vitejs.dev/) - Front-end build tool
+
+
+## Author
+
+- Website - [creeve.me](https://creeve.me)
+- Frontend Mentor - [@zynth17](https://www.frontendmentor.io/profile/zynth17)
+- Twitter - [@hi_reeve](https://twitter.com/hi_reeve)
+- Instagram - [@hi_reeve](https://www.instagram.com/hi_reeve/)
+- Github - [zynth17](https://github.com/zynth17)
+
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import style from '@/components/Footer.module.scss'
 
+import FacebookIcon from '@/assets/images/icon-facebook.svg'
+import InstagramIcon from '@/assets/images/icon-instagram.svg'
+import PinterestIcon from '@/assets/images/icon-pinterest.svg'
+import TwitterIcon from '@/assets/images/icon-twitter.svg'
 const Footer = () => {
 	return (
-		<div className="w-full bg-violet-dark flex ">
-			<h3 className="mt-3 text-center font-bold text-xl">Shortly</h3>
+		<div className={ style.footer}>
+			<h3 className={style.footer__title}>Shortly</h3>
+			<div className={style['footer__link-wrapper']}>
+				<span className={style['footer__link-title']}>Features</span>
+				<span className={style['footer__link'] }>Link Shortening</span>
+				<span className={style['footer__link'] }>Branded Links</span>
+				<span className={style['footer__link'] }>Analytics</span>
+			</div>
+			<div className={style['footer__link-wrapper']}>
+				<span className={style['footer__link-title']}>Resources</span>
+				<span className={style['footer__link'] }>Blog</span>
+				<span className={style['footer__link'] }>Developers</span>
+				<span className={style['footer__link'] }>Support</span>
+			</div>
+			<div className={style['footer__link-wrapper']}>
+				<span className={style['footer__link-title']}>Company</span>
+				<span className={style['footer__link'] }>About</span>
+				<span className={style['footer__link'] }>Our Team</span>
+				<span className={style['footer__link'] }>Careers</span>
+				<span className={style['footer__link'] }>Contact</span>
+			</div>
+			<div className={style['footer__link-socmed']}>
+				<img src={FacebookIcon} className={style['footer__link-socmed--icon'] } />
+				<img src={TwitterIcon} className={style['footer__link-socmed--icon'] } />
+				<img src={PinterestIcon} className={style['footer__link-socmed--icon'] } />
+				<img src={InstagramIcon} className={style['footer__link-socmed--icon'] } />
+			</div>
 		</div>
 	)
 }
